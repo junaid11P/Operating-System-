1a862e58c5eda559ae9ad2f7273beccc6c331206
Juned <79745275+junaid11P@users.noreply.github.com>
2025-02-12 21:58:05 +0530

    thread

--- Thread.c
+++ Thread.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
+#include<pthread.h>
 void* fun(void *arg)
 {
+    
     printf("Hello World\n");
-
+    
+   
     pthread_exit(NULL);
 }
-
-int main(int argc, char** argv){
-    pthread_t t_id[3];
+int main(int argc, char** argv) {
+ pthread_t t_id[3];
 
     int i=0;
     for(i=0;i<3;i++)
     {
-        int x =pthread_create(&t_id[i],NULL,fun,NULL);
-        if(x==0)
-        {
-            printf("Thread %d created successfully\n",(i+1));
-        }
-        else
-        {
-            printf("Thread creation failed\n");
-        }
-        printf("Thread ID: %ld\n",t_id[i]);
+    int x=pthread_create(&t_id[i],NULL,fun,NULL);
+     if(x==0)
+    {
+        printf("Thread %d created successfully\n",(i+1));
+        
     }
+    else
+    {
+          printf("Failed to created thread\n");
+    }
+     printf("Thread id is %d\n",t_id[i]);
+    }
+    
     return (EXIT_SUCCESS);
 }
-
