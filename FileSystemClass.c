932d086e1d3d5a0c41e567b2329e93e7e943cfbe
Juned <79745275+junaid11P@users.noreply.github.com>
2025-02-12 19:04:11 +0530

    file

--- FileSystemClass.c
+++ FileSystemClass.c
@@ -4,5 +4,25 @@
 
 int main(){
     char buf[]=" ";
-    int fd1 = open("")
+    int fd1 = open("CSE.text",O_RDONLY);
+    int fd2 = open("ISE.text",O_WRONLY);
+
+    if(fd1 == -1){
+        printf("Error opening first file\n");
+        return 1;
+    }
+    if(fd2 == -1){
+        printf("Error opening second file\n");
+        return 1;
+    }
+
+    while(read(fd1, buf, 1)){
+        write(fd2,buf,1);
+    }
+    printf("copied successfully\n");
+
+    close(fd1);
+    close(fd2);
+    return 0;
+
 }
